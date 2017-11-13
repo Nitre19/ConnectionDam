2c102f93c3d25aacc32f3f3111d26850569c5ea5
Arnau Tarragó <arnautarragollorach@gmail.com>
2017-11-13 14:24:50 +0100

    Hola.

---
+++ ConnectionDam/FrmMain.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConnectionDam
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            ClSocket socket = new ClSocket();
+
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    //return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+
+            //socket.connectSocketListener();
+        }
+
+        private void btConectar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
